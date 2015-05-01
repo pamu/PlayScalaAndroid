f04036bb993f810c4e3cd64b78bf5db7664d68ba
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-05-01 15:31:40 +0530

    added cars

--- app/global/Global.scala
+++ app/global/Global.scala
@@ -15,11 +15,11 @@ object Global extends GlobalSettings {
     super.onStart(app)
     Logger.info("PlayScalaAndroid Started")
     import actors.CarStore._
-    carStore ! AddCar(name = "swif", url = "")
-    carStore ! AddCar(name = "swif", url = "")
-    carStore ! AddCar(name = "swif", url = "")
-    carStore ! AddCar(name = "swif", url = "")
-    carStore ! AddCar(name = "swif", url = "")
+    carStore ! AddCar(name = "swif", url = "http://i.ndtvimg.com/auto/makers/29/205/suzuki-swift-2011-front-view.jpg")
+    carStore ! AddCar(name = "martui", url = "http://i.ndtvimg.com/auto/makers/29/196/2012-maruti-suzuki-ertiga-001-1570.jpg")
+    carStore ! AddCar(name = "tesla", url = "http://image.motortrend.com/f/oftheyear/car/1301_2013_motor_trend_car_of_the_year_tesla_model_s/41007734/2013-tesla-model-s-front-1.jpg")
+    carStore ! AddCar(name = "bmw", url = "http://2015bestsuvreviews.com/wp-content/uploads/2014/08/2017-BMW-X3-front.jpg")
+    carStore ! AddCar(name = "nano", url = "http://www.igyaan.in/wp-content/uploads/2013/06/tata-nano-exterior-047.jpg")
   }
   override def onStop(app: Application): Unit = {
     super.onStop(app)
